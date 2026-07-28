b111f7653f22c03143d72c67c752828d267704ac
Anuta <antoinettes.s888@gmail.com>
2026-07-28 11:25:54 +0300

    -добавила клик по буквам -добавила модальнон окно -валидация для контактов -переписала API- запросы -сделала состояния загрузки и ошибки

--- api.js
+++ api.js
@@ -1,22 +1,31 @@
-function loadData(url, containerData){
-    fetch(url)
-    .then(function(response){
-        response.json().then(function(data){
-            const limited = data.slice(0, 10)
-            const container = document.getElementById(containerData)
-            container.innerHTML = ''
+ async function loadData(url, containerId){
+    const container = document.getElementById(containerId)
+    container.innerHTML = '<p style="color: #95066e;"> Загрузка...</p>'
+    try{
+        const response = await fetch(url)
+        if(!response.ok){
+            throw new Error(`HTTP error! status: ${response.status}`)
+        }
+        const data = await response.json()
+        const limited = data.slice(0, 10)
+        const container = document.getElementById(containerId)
+        container.innerHTML = ''
 
-            for(let i = 0; i<limited.length; i++){
-                const item = limited[i]
-                container.innerHTML = container.innerHTML + (item.name || item.title) + '<br>'
-            }
-        
-                })
-    })
-    .catch(function(error){
-        console.log('Ошибка')
-    })
-}
+        for(let i = 0; i < limited.length; i++){
+            const item = limited[i]
+            container.innerHTML += 
+            `<div class="api-item">
+                    <strong>${item.id}. ${item.name || item.title}</strong>
+                </div>`
+        }
+        console.log(`Данные загружены из: ${url}`)
+    
+    } catch(error){
+        console.error(`Ошибка при загрузке данных из ${url}:`, error.message)
+        container.innerHTML = `<p style="color: red;"> Ошибка: ${error.message}</p>`;
+    }
+ }
+    
 
 loadData('https://jsonplaceholder.typicode.com/users', 'usersList');
 loadData('https://jsonplaceholder.typicode.com/posts', 'postsList');
--- script.js
+++ script.js
@@ -1,26 +1,99 @@
 let contacts = [];
-let nextId = 1;
+let currentLetter = null
+
+function checkName(name){
+    name = name.trim()
+    if(name.length < 2){
+        alert('Имя должно быть не менее 2 символов')
+        return false
+    }
+    for (let i = 0; i < name.length; i++) {
+        let char = name[i];
+        if (!isLetter(char) && char !== ' ') {
+            alert('Имя может содержать только буквы и пробелы');
+            return false;
+        }
+    }
+    return true
+}
+
+function checkJob(job){
+    job = job.trim()
+    if(job ===''){
+        alert('Должность не может быть пустой')
+        return false
+    }
+    for(let i=0; i< job.length; i++){
+        let char = job[i]
+        if(!isLetter(char) && char !== ' '){
+            alert('Должность может содержать только буквы и пробелы')
+            return false
+        }
+    }
+    return true
+}
+
+function checkPhone(phone){
+    let digits = ''
+    for(let i = 0; i< phone.length; i++){
+        if(phone[i] >= '0' && phone[i] <= '9'){
+            digits += phone[i]
+        }
+    }
+    if(digits.length !== 11){
+        alert('Телефон должен содержать 11 цифр (например, 79123456789)')
+        return false
+    }
+     if (digits[0] !== '7' && digits[0] !== '8'){
+        alert('Телефон должен начинаться с 7 или 8')
+        return false
+     }
+    return true
+}
+
+function isLetter(char){
+    return char.toLowerCase() !== char.toUpperCase()
+}
 
 loadContacts()
 
-function addContact(name, job, phone){
-   const newContact = {
-    id : nextId,
-    name : name,
-    job : job,
-    phone : phone
-   };
+function updateUI() {
+    saveContacts();
+    render();
+    renderAlphabet();
+}
 
- contacts.push(newContact);
- nextId++;
- saveContacts()
+function addContact(name, job, phone) {
+    if (!checkName(name)) return;
+    if (!checkJob(job)) return;
+    if (!checkPhone(phone)) return;
+    
+    const newContact = {
+        id: Date.now(),
+        name: name.trim(),
+        job: job.trim(),
+        phone: phone 
+    };
+
+    contacts.push(newContact);
+    nextId++;
+    updateUI();
 }
 
 
 function render(){
+  
+    let filteredContacts = []
+   for( let i = 0; i < contacts.length; i++){
+    if(currentLetter === null || contacts[i].name[0].toUpperCase() === currentLetter){
+        filteredContacts.push(contacts[i])
+    }
+   }
+   
+   
    const container = document.getElementById('contactsList');
    container.innerHTML = '';
-   contacts.forEach(function(contact){
+   filteredContacts.forEach(function(contact){
     container.innerHTML +=
     `<div class='contact-card'>
         <p class='contact-name'>${contact.name}</p>
@@ -36,6 +109,7 @@ render()
 const alphabet = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'.split('');
 
 function renderAlphabet(){
+
     const container = document.getElementById('alphabet');
     container.innerHTML='';
 
@@ -48,12 +122,30 @@ function renderAlphabet(){
                 count++
             }
         }
-        container.innerHTML += `<button>${letter} (${count})</button>`;
-    }
+       const button = document.createElement('button');
+       button.textContent = `${letter} (${count})`
+       button.addEventListener('click' , function(){
+        currentLetter = letter;
+        render();
+    
+    })
+    container.appendChild(button)
 }
+}
+
 renderAlphabet();
 
 
+function openModal(content){
+    const overlay = document.getElementById('modalOverlay')
+    const body = document.getElementById('modalBody')
+    body.innerHTML = content
+    overlay.style.display = 'flex'
+}
+
+function closeModal(){
+    document.getElementById('modalOverlay').style.display = 'none'
+}
 document.getElementById('addBtn').addEventListener('click', function(){
     const name = document.getElementById('nameInput').value;
     const job = document.getElementById('positionInput').value;
@@ -66,9 +158,7 @@ document.getElementById('addBtn').addEventListener('click', function(){
         document.getElementById('positionInput').value = '';
         document.getElementById('phoneInput').value = '';
        
-        render();
-        renderAlphabet();
-        saveContacts()
+        updateUI()
     }else{
         alert('Заполни поля!')
     } 
@@ -76,9 +166,7 @@ document.getElementById('addBtn').addEventListener('click', function(){
 
 document.getElementById('deleteAllBtn').addEventListener('click', function(){
    contacts = []
-   saveContacts()
-   render()
-   renderAlphabet() 
+   updateUI()
 });
 
 function deleteContact(id){
@@ -92,9 +180,7 @@ function deleteContact(id){
 
     if(index !== -1){
         contacts.splice(index, 1);
-        saveContacts()
-        render();
-        renderAlphabet();
+        updateUI();
     }
 }
 
@@ -104,61 +190,106 @@ function editContact(id) {
         if (contacts[i].id === id) {
             const contact = contacts[i];
             
-            const newName = prompt('Введите новое имя:', contact.name);
-            if (newName !== null && newName !== '') {
-                contact.name = newName;
-            }
-            const newJob  = prompt('Введите новую должность:', contact.job);
-            if(newJob !== null && newJob !== ''){
-                contact.job = newJob;
-            }
-            const newPhone  = prompt('Введите новый телфон:', contact.phone);
-            if(newPhone !== null && newPhone !== ''){
-                contact.phone = newPhone;
+             const html = `
+                <h2>✏️ Редактировать контакт</h2>
+                <input type="text" id="editName" value="${contact.name}" placeholder="Имя">
+                <input type="text" id="editJob" value="${contact.job}" placeholder="Должность">
+                <input type="text" id="editPhone" value="${contact.phone}" placeholder="Телефон">
+                <div class="btn-group">
+                    <button class="btn-secondary" onclick="closeModal()">Отмена</button>
+                    <button class="btn-primary" onclick="saveEdit(${contact.id})">Сохранить</button>
+                </div>
+            `;
+            
+            // Открываем модальное окно с этой формой
+            openModal(html);
+            return; // выходим, чтобы не искать дальше
         }
-       saveContacts()
-       render();
-       renderAlphabet();
-       return;
     }
-  }
 }
 
+function saveEdit(id){
+    const name = document.getElementById('editName').value;
+    const job = document.getElementById('editJob').value;
+    const phone = document.getElementById('editPhone').value;
 
-document.getElementById('searchBtn').addEventListener('click', function(){
-  const query = prompt('Введите свои данные') 
-  if( query === null || query ===''){
-    return
-  } 
-  const results = []
-  for (let i = 0; i< contacts.length; i++){
-    const contact = contacts[i]
-    if(contact.name.includes(query) ||
-       contact.job.includes(query) ||
-       contact.phone.includes(query)) {
-       results.push(contact);
-  }
+    if (!checkName(name)) return;
+    if (!checkJob(job)) return;
+    if (!checkPhone(phone)) return;
+
+  
+    for (let i = 0; i < contacts.length; i++) {
+        if (contacts[i].id === id) {
+            contacts[i].name = name.trim();
+            contacts[i].job = job.trim();
+            contacts[i].phone = phone;
+            break;
+        }
+    }
+
+    updateUI();
+    closeModal();
 }
 
-const container = document.getElementById('contactsList')
-container.innerHTML = ''
+document.getElementById('searchBtn').addEventListener('click', function() {
+    const html = `
+        <h2>🔍 Поиск контактов</h2>
+        <input type="text" id="searchInput" placeholder="Введите имя, должность или телефон...">
+        <div class="btn-group">
+            <button class="btn-secondary" onclick="closeModal()">Закрыть</button>
+            <button class="btn-primary" onclick="searchContacts()">Найти</button>
+            <button class="btn-secondary" onclick="resetSearch()">Сбросить</button>
+        </div>
+        <div id="searchResults" class="search-results"></div>
+    `;
+    openModal(html);
+});
 
-if(results.length ===0){
-    return
+function searchContacts() {
+    const query = document.getElementById('searchInput').value.toLowerCase()
+    const results = []
+
+    for(let i = 0; i < contacts.length;i++){
+        const contact = contacts[i]
+        if( contact.name.toLowerCase().includes(query) ||
+            contact.job.toLowerCase().includes(query) ||
+            contact.phone.toLowerCase().includes(query)){
+                results.push(contact)
+            }
+    }
+
+  const container = document.getElementById('searchResults');
+    container.innerHTML = '';
+    
+    if (results.length === 0) {
+        container.innerHTML = '<p>Ничего не найдено</p>';
+        return;
+    }
+    
+    for (let i = 0; i < results.length; i++) {
+        const contact = results[i];
+        container.innerHTML += `
+            <div class="result-item">
+                <span class="result-name">${contact.name}</span>
+                <span>${contact.job}</span>
+                <span>${contact.phone}</span>
+            </div>
+        `;
+    }
 }
 
-for(let i =0; i < results.length; i++){
-    const contact = results[i]
-    container.innerHTML+=`
-    <div class="contact-card">
-                <p class="contact-name">${contact.name}</p>
-                <p class="contact-job">${contact.job}</p>
-                <p class="contact-phone">${contact.phone}</p>
-                <button onclick="deleteContact(${contact.id})">🗑️ Удалить</button>
-                <button onclick="editContact(${contact.id})">✏️ Редактировать</button>
-            </div>`
+function resetSearch() {
+    const input = document.getElementById('searchInput')
+    if(input) input.value = ''
+    document.getElementById('searchResults').innerHTML = ''
 }
-})
+
+document.getElementById('closeModalBtn').addEventListener('click', closeModal)
+    document.getElementById('modalOverlay').addEventListener('click', function(event) {
+    if (event.target === this) {
+        closeModal();
+    }
+});
 
 function saveContacts(){
     localStorage.setItem('contacts', JSON.stringify(contacts))
@@ -169,9 +300,6 @@ function loadContacts() {
         const saved = JSON.parse(data);
         for (let i = 0; i < saved.length; i++) {
             contacts.push(saved[i]);
-        }
-        if (contacts.length > 0) {
-            nextId = contacts[contacts.length - 1].id + 1;
         }
     }
 }
